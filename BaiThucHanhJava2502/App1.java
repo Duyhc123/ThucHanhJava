93bb8fa9bd97f1b1163568211415c64cbc35c331
Duyhc123 <duy123.lqm@gmail.com>
2022-03-04 12:26:48 +0700

    '2502'

--- BaiThucHanhJava2502/App1.java
+++ BaiThucHanhJava2502/App1.java
@@ -3,9 +3,9 @@ public class App1 {
 public static void main(String[] args) {
     int a, b;
     try (Scanner scanner = new Scanner(System.in)) {
-        System.out.println("Nhap vao so nguyen duong a :");
+        System.out.println("Nhap vao so nguyen duong a :") ;
         a = scanner.nextInt();
-        System.out.println("Nhap vao so nguyen duong b :");
+        System.out.println("Nhap vao so nguyen duong b :") ;
         b = scanner.nextInt();
     System.out.println("UCLN cua " + a + " va " + b
                 + " la: " + UCLN(a, b));}
