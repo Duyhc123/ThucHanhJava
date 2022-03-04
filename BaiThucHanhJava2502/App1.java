3f1df40b945e04fef132e376c733af0c48e9789c
Duyhc123 <duy123.lqm@gmail.com>
2022-03-04 12:35:54 +0700

    2502

--- BaiThucHanhJava2502/App1.java
+++ BaiThucHanhJava2502/App1.java
@@ -12,7 +12,7 @@ public static void main(String[] args) {
     }
     public static int UCLN(int a,int b) {
     if (b == 0) return a;
-        return UCLN(b, a % b);
+        return UCLN(b, a % b) ;
     }
 }
 
